4cefd81767169d01a12ebdcc2dd4faa7f6f5b600
ubuntu <mr.shahbaz.aslam@gmail.com>
2018-12-18 15:53:17 +0400

    8. Set minimum chain work to 0x00 & 9. Change the default port

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -103,7 +103,7 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1517356801; // January 31st, 2018
 
         // The best chain should have at least this much work.
-        consensus.nMinimumChainWork = uint256S("0x0000000000000000000000000000000000000000000000c1bfe2bbe614f41260");
+        consensus.nMinimumChainWork = uint256S("0x0000000000000000000000000000000000000000000000000000000000000000");
 
         // By default assume that the signatures in ancestors of this block are valid.
         consensus.defaultAssumeValid = uint256S("0x66f49ad85624c33e4fd61aa45c54012509ed4a53308908dd07f56346c7939273"); //1441280
@@ -117,7 +117,7 @@ public:
         pchMessageStart[1] = 0x45;
         pchMessageStart[2] = 0x43;
         pchMessageStart[3] = 0x48;
-        nDefaultPort = 9333;
+        nDefaultPort = 9919;
         nPruneAfterHeight = 100000;
 
         genesis = CreateGenesisBlock(1545131250, 2095775697, 0x1e0ffff0, 1, 50 * COIN);
