46595651ab893d7935525cc269f8e4348ccfd561
ubuntu <mr.shahbaz.aslam@gmail.com>
2018-12-19 16:44:45 +0400

    Test new Message

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -49,7 +49,7 @@ static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesi
  */
 static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
-    const char* pszTimestamp = "BBC NEWS 20/Dec/2017 Bitcoin Cash deals frozen as insider trading is probed";
+    const char* pszTimestamp = "NY Times 19/Dec/2018 As Facebook Raised a Privacy Wall, It Carved an Opening for Tech Giants";
     const CScript genesisOutputScript = CScript() << ParseHex("040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9") << OP_CHECKSIG;
     return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTime, nNonce, nBits, nVersion, genesisReward);
 }
@@ -119,15 +119,15 @@ public:
         nDefaultPort = 9919;
         nPruneAfterHeight = 100000;
 
-        genesis = CreateGenesisBlock(1513784917, 2084820399, 0x1e0ffff0, 1, 50 * COIN);
+        genesis = CreateGenesisBlock(1545220912, 2085418343, 0x1e0ffff0, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-	/*
+	
 	//Found GenesisBlock and MerkleRoot that way :P	
 	printf("%s\n", consensus.hashGenesisBlock.ToString().c_str());
 	printf("%s\n", genesis.hashMerkleRoot.ToString().c_str());
-	*/
-	assert(consensus.hashGenesisBlock == uint256S("0xee686214586277080977cf43d2a0b38bcd48d696129b19c026d8208f9e745cea"));
-        assert(genesis.hashMerkleRoot == uint256S("0x805d8d176abe872f5cd40b558869a0d25ed02ae471fa33f80185443e5ca9cb40"));
+	
+	assert(consensus.hashGenesisBlock == uint256S("0x45898f2d2f2614b0951d89db17221c3d658aa1519b3366c049aa34f18b209108"));
+        assert(genesis.hashMerkleRoot == uint256S("0x2b5cb9db56072e752e8f27a893dd433584ea3b45b6ed8533094422b3deb168e1"));
         // Note that of those with the service bits flag, most only support a subset of possible options
         //vSeeds.emplace_back("seed-a.techiecoin.loshan.co.uk");
         //vSeeds.emplace_back("dnsseed.thrasher.io");
@@ -152,7 +152,7 @@ public:
 
         checkpointData = {
             {
-		{  0, uint256S("0xee686214586277080977cf43d2a0b38bcd48d696129b19c026d8208f9e745cea")},
+		{  0, uint256S("0x45898f2d2f2614b0951d89db17221c3d658aa1519b3366c049aa34f18b209108")},
 		/*
                 {  1500, uint256S("0x841a2965955dd288cfa707a755d05a54e45f8bd476835ec9af4402a2b59a2967")},
                 {  4032, uint256S("0x9ce90e427198fc0ef05e5905ce3503725b80e26afd35a987965fd7e3d9cf0846")},
@@ -176,7 +176,7 @@ public:
 
         chainTxData = ChainTxData{
             // Data as of block 59c9b9d3fec105bdc716d84caa7579503d5b05b73618d0bf2d5fa639f780a011 (height 1353397).
-            1513784917, // * UNIX timestamp of last known number of transactions
+            1545220912, // * UNIX timestamp of last known number of transactions
             0,  // * total number of transactions between genesis and that timestamp
                     //   (the tx=... number in the SetBestChain debug.log lines)
             0.0     // * estimated number of transactions per second after that timestamp
